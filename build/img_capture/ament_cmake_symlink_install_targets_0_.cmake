540fcbb5b2b5c2b7e7da9554dcde6521f0723d6a
Xprize Snowflake <xprizesnowflake@gmail.com>
2025-01-29 09:04:24 -0700

    added odometry node to package

---
+++ build/img_capture/ament_cmake_symlink_install_targets_0_.cmake
@@ -0,0 +1,1 @@
+ament_cmake_symlink_install_targets("TARGET_FILES" "/home/username/ros2_ws/build/img_capture/seekcamera_publisher" "TARGETS" "seekcamera_publisher" "DESTINATION" "lib/img_capture")
--- install/img_capture/share/img_capture/launch/img_capture.launch.py
+++ install/img_capture/share/img_capture/launch/img_capture.launch.py
@@ -1,18 +1,1 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-
-def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='img_capture',
-            executable='seekcamera_publisher',
-            name='seekcamera_publisher',
-            output='screen'
-        )
-	Node(
-	    package='img_capture',
-	    executable='thermal_subscriber',
-	    name='thermal_subscriber',
-	    output='screen'
-	)
-    ])
+/home/username/ros2_ws/src/img_capture/launch/img_capture.launch.py
