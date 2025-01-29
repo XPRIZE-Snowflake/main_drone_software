fc7609d068679071ad07cd7f9dc198e9ac6c9639
isaacaarondavies <isaac151@byu.edu>
2025-01-29 09:05:36 -0700

    Moved and updated kmeans

--- src/img_capture/scripts/kmeans.py
+++ src/img_capture/scripts/kmeans.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
+
 import rclpy
 from rclpy.node import Node
 import numpy as np
-from detection.msg import Coords, Clusters
-
+from px4_msgs.msg import VehicleCommand
+import math
 
 class DynamicKMeans(Node):
   def __init__(self, max_radius, min_size=1):
@@ -17,8 +19,26 @@ class DynamicKMeans(Node):
     self.elements = np.empty((0, 2))  # Start with no elements
     self.labels = []  # Store labels for the most recent batch of added elements
 
-    self.create_subscription(Pixels, '/hot_spots', self.add_elements) #FIXME: Subscribe to correct topic
-    self.pub = self.create_publisher(Clusters, '/clusters', 10); #FIXME: find out what to publish
+    self.create_subscription(String, '/thermal_with_odom', self.add_elements)
+    self.pub = self.create_publisher(VehicleCommand, '/fmu/in/vehicle_command', 10)
+
+  def send_command(self):
+    msg = VehicleCommand()
+    msg.command = 16  # MAV_CMD_NAV_WAYPOINT
+    msg.param1 = 0.0  # Hold time at waypoint
+    msg.param2 = 5.0  # Acceptance radius (meters)
+    msg.param3 = 0.0  # Pass radius
+    msg.param4 = math.nan  # Yaw angle (don’t care)
+    msg.param5 = 47.3977417  # Latitude
+    msg.param6 = 8.5455939   # Longitude
+    msg.param7 = 10.0  # Altitude (meters)
+    msg.target_system = 1
+    msg.target_component = 1
+    msg.source_system = 1
+    msg.source_component = 1
+    msg.confirmation = 0
+    self.publisher.publish(msg)
+    self.get_logger().info(f’Sent mission command to {msg.param5}, {msg.param6}, {msg.param7}’)
 
   def add_elements(self, elements):
     """
@@ -57,6 +77,13 @@ class DynamicKMeans(Node):
 
     self.centroids = np.array(new_centroids)
 
+  def convert_to_coords(self):
+    """
+    Convert the current centroids to a list of Coords messages.
+    :return: A list of Coords messages.
+    """
+    return [Coords(x=self.centroids[i, 0], y=self.centroids[i, 1]) for i in range(self.centroids.shape[0])]
+
   def get_centroids(self):
     """
     Get the current centroids with at least min_size elements.
@@ -69,7 +96,7 @@ class DynamicKMeans(Node):
     counts = np.bincount(self.labels, minlength=self.centroids.shape[0])
     filtered_centroids = self.centroids[counts >= self.min_size]
 
-    return filtered_centroids
+    self.send_command(filtered_centroids)
 
 def main(args=None):
     rclpy.init(args=args)
