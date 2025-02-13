6bb4339efe28f4c242f058dd4c042a0394d520b5
Xprize Snowflake <xprizesnowflake@gmail.com>
2025-02-13 09:57:48 -0700

    adding drop mech

--- src/img_capture/scripts/drop_mech.py
+++ src/img_capture/scripts/drop_mech.py
@@ -3,7 +3,7 @@
 ## opens drop mechanism when receives command
 # changes servo value/angle when it recieves true from dropcommand topic
 
-# TODO: pigpio library not installing. need to fix
+# TODO: manually open the drop mech
 
 # import os
 import math
@@ -36,13 +36,18 @@ class DropMechNode(Node):
         self.y = 0
         self.alt = 0
 
-        # self.servo = Servo(25)
-        self.openVal = -1
-        self.closeVal = 0
+        self.servo = Servo(25)
+        self.openVal = 0.7
+        self.closeVal = -1
+        self.servo.value = self.closeVal
 
         ## Subscribers ##
         self.img_sub = self.create_subscription(
             String, "hotspot_info", self.drop_callback, 10
+        )
+
+        self.img_sub = self.create_subscription(
+            String, "servo_command", self.servo_command_callback, 10
         )
 
     def drop_callback(self, msg):
@@ -52,18 +57,27 @@ class DropMechNode(Node):
             self.y = data.get("y", 0)
             self.alt = data.get("alt", 70)
             self.hot_location = data
-            if math.abs(self.x) < 5 and math.abs(self.y) < 5:
+            if self.x <  5 and self.y < 5:
                 self.get_logger().info(f"Drop mech activated")
-                # self.servo.value = self.val
+                self.servo.value = self.openVal
         except Exception as e:
             self.get_logger().error(f"Failed to parse hotspot data in mech drop code: {e}")
-        # self.hot_location = msg
-        # # probably use json to unpack data
-        # if msg.data is True:
-        #     ## figure out what position to move the servo to
-        #     #servo.min()
-        #     #servo.max()
-        #     servo.value = 0.5
+
+    def servo_command_callback(self, msg):
+        try:
+            data = json.loads(msg.data)
+            self.newAngle = msg.data
+            self.get_logger().info(f"topic data: {self.newAngle}")
+            if(self.newAngle == "0.7"):
+                self.servo.value = self.openVal
+            # if(self.newAngle == "open"):
+            #     self.servo.value = self.openVal
+            # elif (self.newAngle == "close"):
+            #     self.servo.value = self.closeVal
+            
+
+        except Exception as e:
+            self.get_logger().info(f"Fail to parse user command: {e}")
 	
 
 def main(args=None):
